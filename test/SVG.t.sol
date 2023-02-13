679281e5e313b1c125846b0751d835ba45768c8c
nicholas <nnnnicholas@users.noreply.github.com>
2023-02-13 15:49:55 -0500

    ffi warning

--- test/SVG.t.sol
+++ test/SVG.t.sol
@@ -11,11 +11,13 @@ contract EmptyTest is Test {
 
     function testOutput() public {
         string memory returnedUri = svgContract.tokenUri(1);
+
+        // The following lines open the SVG in your default SVG reader. This part of the code depends upon `ffi`. Note: `ffi` is not safe in repos where untrusted parties can mutate the code (incl PRs). If a malicious party introduces code and the dev runs it using `ffi`, it is executed by node and has full disk access. In sum: if you're working on a shared codebase that others can push to, or are running tests on a PR by an untrusted party, consider removing the `ffi` code below.
         string[] memory inputs = new string[](3);
         inputs[0] = "node";
         inputs[1] = "./open.js";
         inputs[2] = returnedUri;
-        bytes memory res = vm.ffi(inputs);
-        // vm.ffi(inputs);
+        // bytes memory res = vm.ffi(inputs); // Complains that res is not used
+        vm.ffi(inputs);
     }
 }
